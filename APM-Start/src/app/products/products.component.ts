223fff4ac141772b6d120b18cafc3295a20c72b5
a1633661 <oscarsalem.garcia@student.adelaide.edu.au>
2018-02-04 22:47:53 +0800

    more functionalities

--- APM-Start/src/app/products/products.component.ts
+++ APM-Start/src/app/products/products.component.ts
@@ -11,6 +11,7 @@ export class ProductsComponent implements OnInit {
    imageWidth: number = 50;
    imageMargin: number = 2;
    showImage: boolean = false;
+   listFilter: string ='cart';
    products: any[] = [
     {
         "productId": 1,
